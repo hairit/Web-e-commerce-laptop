9b2f5ecb171ffc8315b30495e05f2c406ba3dc89
lthoa462 <lthoa462@gmail.com>
2021-10-26 21:02:47 +0700

    sua sai

--- laptop-store-webapp/src/Pages/Product.js
+++ laptop-store-webapp/src/Pages/Product.js
@@ -1,5 +1,5 @@
 import React from "react";
 import "../CSS/Product.css";
 export default function Product() {
-  return <div className="product">Quốc Đạt đẹp trai số một thế giới</div>;
+  return <div className="product">Có cl Quốc Đạt đẹp trai số một thế giới</div>;
 }
