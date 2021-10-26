5f3723a2e6b9b0e4e0488a1debd2d16e7c89fbea
quocdat202 <quocdat732@gmail.com>
2021-10-26 20:55:19 +0700

    Edit prodcut

--- laptop-store-webapp/src/Pages/Product.js
+++ laptop-store-webapp/src/Pages/Product.js
@@ -1,9 +1,5 @@
-import React from 'react'
-import '../CSS/Product.css'
+import React from "react";
+import "../CSS/Product.css";
 export default function Product() {
-    return (
-        <div className="product">
-           Lol Đạt
-        </div>
-    )
+  return <div className="product">Quốc Đạt đẹp trai số một thế giới</div>;
 }
