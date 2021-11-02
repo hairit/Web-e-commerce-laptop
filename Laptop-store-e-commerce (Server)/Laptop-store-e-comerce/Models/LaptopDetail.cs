2d65f2a08a182adb6c0fc809f4524df58214550a
unknown <tuonghai.contact@gmail.com>
2021-11-02 15:33:31 +0700

    Update back end

---
+++ Laptop-store-e-commerce (Server)/Laptop-store-e-comerce/Models/LaptopDetail.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Laptop_store_e_comerce.Models
+{
+    public class LaptopDetail
+    {
+        private ThongSoLaptop inf;
+        private MoTaLaptop des;
+        LaptopDetail(ThongSoLaptop inf ,MoTaLaptop des)
+        {
+            this.Inf = inf;
+            this.Des = des;
+        }
+
+        public ThongSoLaptop Inf { get => inf; set => inf = value; }
+        public MoTaLaptop Des { get => des; set => des = value; }
+    }
+}
